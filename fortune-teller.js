aeba732e0653d59feb2c76e66df14900253794f5
andyding <andy.ding89@gmail.com>
2016-02-02 21:02:00 +0000

    implemented process.argv array

--- fortune-teller.js
+++ fortune-teller.js
@@ -1,3 +1,14 @@
 var fortune = require('./library/fortune.js');
 
-console.log(fortune.getFortune());
+
+if(process.argv[2] === undefined){
+    console.log(fortune.getFortune());
+}
+
+else{
+    var i = 0;
+    while(i<process.argv[2]){
+        console.log(fortune.getFortune());
+        i++;
+    }
+}
