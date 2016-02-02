7e3dc46c4db5e3306d29280c44a330aa88b65e32
andyding <andy.ding89@gmail.com>
2016-02-02 19:39:31 +0000

    Finished CPS code

---
+++ cps.js
@@ -0,0 +1,30 @@
+//CPS function to return first character of a string
+function firstChar(string, func){
+    func(string[0]);
+}
+
+firstChar("vsdg",function(firstLetter){
+    console.log(firstLetter);
+});
+
+//CPS function to return last character of a string
+function lastChar(string, func){
+    func(string[string.length-1]);
+}
+
+lastChar("asdf", function(lastLetter){
+    console.log(lastLetter);
+});
+
+//Create a function that takes a string and a continuation (callback) to use two previous functions to return a string that contains both the first and last character of the initial string
+function getFirstAndLast(string, callback){
+    firstChar(string,function(firstLetter){
+        lastChar(string,function(lastLetter){
+            callback(firstLetter.concat(lastLetter));  
+        });
+    });
+}
+
+getFirstAndLast("hello",function(newStr){
+    console.log(newStr);
+});
